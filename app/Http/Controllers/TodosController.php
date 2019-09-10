5d09c434468b5cbdb8d0d9d8fac32706f0d8c63e
HosanaAbollo <hosana.abollo2@gmail.com>
2019-09-10 17:08:25 +0200

    Ajout d bootstrap

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// Accès au model
+use App\Todo; 
+
 class TodosController extends Controller
 {
     
     public function index()
     {
-        return view('todos.index');
+
+        // Récupération de tous les todo en BDD et on l'envoie dans la variable $todos
+        $todos = Todo::all();
+
+        return view('todos.index')->with('todos', $todos);
     }
 }
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -4,11 +4,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Inclusion CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
+
+
     <title>Page des Todos</title>
 </head>
 <body>
 
-<h1>Page des Todos</h1>
+
+
+<div class="container">
+
+    <h1 class="text-center my-5">Page des Todos</h1>
+
+    <div class="row justify-content-center">
+
+        <div class="col-md-8 offset-md-2">
+
+        <div class="card card-default">
+
+            <div class="card-header">
+
+                TODO
+
+            </div>
+
+            <ul class="list-group">
+
+            @foreach($todos as $todo)
+
+                <li class="list-group-item">
+                    {{ $todo->nom}}
+
+                </li>
+
+            @endforeach
+
+            </ul>
+
+            </div>
+        
+        </div>
+    
+    </div>
+
+
+
+</div>
     
 </body>
 </html>
